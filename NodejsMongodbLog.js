3740cf966e80ceb150d13be45935feb10703b65b
sans <1816074881@qq.com>
2021-12-12 17:39:33 -0800

    ejs

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app= express()
+const ejs= require('ejs')
 const mongoose = require('mongoose') 
 mongoose.connect('mongodb://172.21.2.236:27017/190110910810');
 const schema={
@@ -18,5 +19,8 @@ app.get("/input",(req,res)=>{
     console.log(req.query)
     const kitty =new mydata({name:req.query.first,health:req.query.second});
     kitty.save()
+    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
+        res.send(str)
+    })
 })
 app.listen(10810)
